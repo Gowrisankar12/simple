11e8ef5683dc5dd0b242c243ad7d99991a592662
Gowrisankar <gowrisankar431@gmail.com>
2022-07-08 03:52:10 +0530

    new project

---
+++ org/test/Rbibank.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class Rbibank extends sample {
+	private void saving() {
+  System.out.println("Interest amount=8%");
+	}
+	public static void main(String[] args) {
+		Rbibank d=new Rbibank();
+		d.saving();
+		d.icicbank();
+		d.indianbank();
+	}
+
+}
---
+++ org/test/sample.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class sample {
+	public void indianbank() {
+   System.out.println("Enter the branch name");
+	}
+	
+	public void icicbank() {
+        System.out.println("Enter the bank id");
+	}
+
+}
